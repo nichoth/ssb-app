92d9100a055fa49398aa895e1d6d36759c972220
nichoth <nichoth@gmail.com>
2019-09-29 16:37:28 -0700

    test whoami

--- test/server.js
+++ test/server.js
@@ -4,6 +4,18 @@ var Server = require('../src/server')
 var server
 test('server', function (t) {
     server = Server()
+    t.ok(server)
+    t.end()
+})
+
+test('whoami', function (t) {
+    var who = server.whoami()
+    console.log('who', who)
+    t.ok(who, 'whoami')
+    t.end()
+})
+
+test('done', function (t) {
     server.close(function () {
         t.end()
     })
